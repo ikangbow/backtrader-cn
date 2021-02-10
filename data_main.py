b5f4d0f84cf65fb91b5a31188c99e2e91a20d5da
ikangbow <ikangbow@outlook.com>
2021-02-10 13:58:57 +0800

    增加#!/usr/bin/python

--- data_main.py
+++ data_main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import gevent.pool
 import gevent.monkey
--- train_main.py
+++ train_main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import backtradercn.strategies.ma as bsm
 import backtradercn.tasks as btasks
