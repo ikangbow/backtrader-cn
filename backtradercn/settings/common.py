b5cef49da5c5773d457db604fdc0548791858e06
ikangbow <ikangbow@outlook.com>
2021-03-06 11:51:09 +0800

    策略基础类完善

--- backtradercn/settings/common.py
+++ backtradercn/settings/common.py
@@ -15,3 +15,7 @@ CN_STOCK_LIBNAME = 'ts_his_lib'
 DAILY_STOCK_ALERT_LIBNAME = 'daily_stock_alert'
 STRATEGY_PARAMS_LIBNAME = 'strategy_params'
 STRATEGY_PARAMS_MA_SYMBOL = 'ma_trend'
+
+# 1、home目录下建立log文件夹以及data/mongodb/log文件夹  全部可编辑
+# 2、rootPath = os.path.abspath(os.path.join(os.getcwd(), "../"))
+
--- backtradercn/strategies/base.py
+++ backtradercn/strategies/base.py
@@ -1,34 +1,29 @@
 # -*- coding: utf-8 -*-
 import backtrader as bt
+from backtradercn.libs.log import get_logger
+import backtradercn.strategies.utils as bsu
+
+logger = get_logger(__name__)
 
 class StrategyBase(bt.Strategy):
     def __init__(self):
         self.order = None
         self.last_operation = "SELL"
-        self.status = "DISCONNECTED"
         self.bar_executed = 0
         self.buy_price_close = None
-        self.soft_sell = False
-        self.hard_sell = False
+        logger.debug('>>Starting strategy...')
 
     def reset_sell_indicators(self):
-        self.soft_sell = False
-        self.hard_sell = False
         self.buy_price_close = None
-
-    def notify_data(self, data, status, *args, **kwargs):
-        self.status = data._getstatusname(status)
 
     def short(self):
         if self.last_operation == "SELL":
             return
-
         return self.sell()
 
     def long(self):
         if self.last_operation == "BUY":
             return
-
         return self.buy()
 
     def notify_order(self, order):
@@ -43,21 +38,23 @@ class StrategyBase(bt.Strategy):
         elif order.status in [order.Completed]:
             if order.isbuy():
                 self.last_operation = "BUY"
-
-                print(order.__dict__)
+                bsu.Utils.log(self.datas[0].datetime.date(),
+                              f'买入==>价格:{order.executed.price},成本==>{order.executed.value:.2f},手续费==>{order.executed.comm:.2f}')
 
             else:  # Sell
                 self.last_operation = "SELL"
                 self.reset_sell_indicators()
+                bsu.Utils.log(self.datas[0].datetime.date(),
+                              f'卖出==>价格:{order.executed.price},成本==>{order.executed.value:.2f},手续费==>{order.executed.comm:.2f}')
             # Sentinel to None: new orders allowed
         elif order.status in [order.Canceled, order.Margin, order.Rejected]:
-            pass
+            bsu.Utils.log(self.datas[0].datetime.date(),
+                          'order Canceled/Margin/Rejected, order_status is %d',
+                           order.status)
         self.order = None
 
     def notify_trade(self, trade):
         if not trade.isclosed:
             return
 
-        color = 'green'
-        if trade.pnl < 0:
-            color = 'red'
+        bsu.Utils.log(self.datas[0].datetime.date(),f'策略收益：毛收益 {trade.pnl:.2f}, 净收益 {trade.pnlcomm:.2f}')
